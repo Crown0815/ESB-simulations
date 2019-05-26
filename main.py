a65a13b740f51a1bd7b8d200ba25608a57f885e9
Felix Kröner <felix.kroener@gmx.de>
2019-05-26 13:44:46 +0200

    Extend simulation main file to include more infos
    Includes now:
    * Inflection point corrected points
    * Maximum slope
    * Shortcuts for all four nano-levers of interest

--- main.py
+++ main.py
@@ -2,19 +2,35 @@ import voltage_response_simulation as sim
 import matplotlib.pyplot as plt
 
 
-potentials = [-0.2 + 0.01 * x for x in range(1, 56, 1)]               # potential range
-radius = sim.r_dna * 3
-length = 100e-9
-line_charge_density = sim.rho_dna * 6
+def create_coordinates_for_tikz(potentials, radius, length, line_charge_density):
+    fluo = sim.normalized_fluorescence(potentials, length, radius, line_charge_density)
+    ip_index = sim.inflection_point(fluo)
 
-fluo = sim.normalized_fluorescence(potentials, length, radius, line_charge_density)
-ip_index = sim.inflection_point(fluo)
+    for pot, fl in fluo.items():
+        print(str(pot) + " " + str(fl) + "\\\\")
 
-for pot, fl in fluo.items():
-    print(str(pot) + " " + str(fl)+"\\\\")
+    ip_x = list(fluo.keys())[ip_index]
+    print("IP = " + str(list(fluo.keys())[ip_index]) + " " + str(list(fluo.values())[ip_index]) + "\\\\")
+    print("IP slope = " + str(sim.inflection_point_slope(fluo)))
 
-print("IP = "+str(list(fluo.keys())[ip_index]) + " " + str(list(fluo.values())[ip_index])+"\\\\")
+    print("------------------------ Inflection point corrected -----------------------")
+    cntr = 0
+    for pot, fl in fluo.items():
+        if cntr % 10 == 0:
+            print(str(pot - ip_x) + " " + str(fl) + "\\\\")
+        cntr += 1
+
+    return fluo
 
 
-plt.plot(list(fluo.keys()), list(fluo.values()), list(fluo.keys())[ip_index], list(fluo.values())[ip_index], 'o')
+pot = [-0.9 + 0.001 * x for x in range(1, 2060, 1)]  # potential range
+
+# fluo = create_coordinates_for_tikz(pot, sim.r_dna, sim.b*sim.bp, sim.rho_dna)
+# fluo = create_coordinates_for_tikz(pot, sim.r_dna, sim.b*sim.bp*2, sim.rho_dna)
+# fluo = create_coordinates_for_tikz(pot, sim.r_dna * 2.8, 50e-9, sim.rho_dna * 4)
+fluo = create_coordinates_for_tikz(pot, sim.r_dna * 3, 100e-9, sim.rho_dna * 6)
+
+for r in range(1, 11):
+    fluo = sim.normalized_fluorescence(pot, sim.b*sim.bp, sim.r_dna * (1+r*0.1), sim.rho_dna)
+    plt.plot(list(fluo.keys()), list(fluo.values()))
 plt.show()
