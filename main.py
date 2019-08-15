d6bd2db19cc2d0af81b4cb1604ce36ba44b36ffb
Felix Kröner <felix.kroener@gmx.de>
2019-08-15 20:20:30 +0200

    Update simulations main with more voltage response curves

--- main.py
+++ main.py
@@ -23,14 +23,19 @@ def create_coordinates_for_tikz(potentials, radius, length, line_charge_density)
     return fluo
 
 
-pot = [-0.9 + 0.001 * x for x in range(1, 2060, 1)]  # potential range
+pot = [-3.9 + 0.001 * x for x in range(1, 5060, 1)]  # potential range
 
 # fluo = create_coordinates_for_tikz(pot, sim.r_dna, sim.b*sim.bp, sim.rho_dna)
 # fluo = create_coordinates_for_tikz(pot, sim.r_dna, sim.b*sim.bp*2, sim.rho_dna)
 # fluo = create_coordinates_for_tikz(pot, sim.r_dna * 2.8, 50e-9, sim.rho_dna * 4)
-fluo = create_coordinates_for_tikz(pot, sim.r_dna * 3, 100e-9, sim.rho_dna * 6)
+fluo = create_coordinates_for_tikz(pot, sim.r_dna * 2.41, 50e-9, sim.rho_dna * 6)
 
-for r in range(1, 11):
-    fluo = sim.normalized_fluorescence(pot, sim.b*sim.bp, sim.r_dna * (1+r*0.1), sim.rho_dna)
-    plt.plot(list(fluo.keys()), list(fluo.values()))
+fluo = sim.normalized_fluorescence(pot, 50e-9, sim.r_dna * 2.41, sim.rho_dna * 4)
+plt.plot(list(fluo.keys()), list(fluo.values()))
+fluo = sim.normalized_fluorescence(pot, 100e-9, sim.r_dna * 3, sim.rho_dna * 6)
+plt.plot(list(fluo.keys()), list(fluo.values()))
+fluo = sim.normalized_fluorescence(pot, 16e-9, sim.r_dna, sim.rho_dna)
+plt.plot(list(fluo.keys()), list(fluo.values()))
+fluo = sim.normalized_fluorescence(pot, 32e-9, sim.r_dna, sim.rho_dna)
+plt.plot(list(fluo.keys()), list(fluo.values()))
 plt.show()
