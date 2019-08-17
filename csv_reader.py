f73f58e9cc6c6fac6f167b807c88c7d64c204f35
Felix Kröner <felix.kroener@gmx.de>
2019-08-17 13:28:18 +0200

    Move specialized csv readers to csv_reader file

---
+++ csv_reader.py
@@ -0,0 +1,105 @@
+import csv
+import statistics
+
+
+class VrmPrinter:
+    def __init__(self):
+        self.voltage = None
+        self.fluorescence = list()
+
+        self.voltages = list()
+        self.fluorescences = list()
+
+    def add(self, voltage: float, fluorescence: float):
+        if self.voltage is not None and self.voltage != voltage:
+            self.voltages.append(self.voltage)
+            self.fluorescences.append(statistics.mean(self.fluorescence))
+            self.fluorescence.clear()
+            self.voltage = None
+
+        self.voltage = voltage
+        self.fluorescence.append(fluorescence)
+
+    def print(self):
+        for v, f in zip(self.voltages, self.fluorescences):
+            print(f'{v}\t\t{f}\\\\')
+
+    def print_normalized(self):
+        max_fluorescence = self.fluorescences[0]
+        min_fluorescence = self.fluorescences[-1]
+        for v, f in zip(self.voltages, self.fluorescences):
+            print(f'{v}\t\t{((f-min_fluorescence)/(max_fluorescence-min_fluorescence))}\\\\')
+            # print(f'{((f-min_fluorescence)/(max_fluorescence-min_fluorescence))}')
+
+    def read(self, path: str, delimiter: str=';', encoding: str = 'iso-8859-1'):
+        with open(path, encoding=encoding) as csv_file:
+            reader = csv.reader(csv_file, delimiter=delimiter)
+            rows = [row for row in reader]
+            line_count = 0
+            for row in rows:
+                if line_count == 0:
+                    print(f'Column names are {", ".join(row)}')
+                else:
+                    self.add(float(row[1]), float(row[2]))
+                line_count += 1
+            print(f'Read {line_count} lines.')
+
+
+class CsvAverage:
+    def __init__(self, encoding='iso-8859-1', delimiter=';'):
+        self.encoding = encoding
+        self.delimiter = delimiter
+        self.average = list()
+        self.sources = list()
+        self.line_count = None
+
+    def add_source(self, path: str):
+        with open(path, encoding=self.encoding) as file:
+            count = sum(1 for _ in csv.reader(file))
+
+        if self.line_count is None:
+            self.line_count = count
+        elif self.line_count != count:
+            raise Exception("Files must contain same number of lines")
+
+        self.sources.append(path)
+
+    def print(self, col1: int = 0, col2: int = 1, x_offset: float = 0, show_column_names: bool = False):
+        files = [open(source, encoding=self.encoding) for source in self.sources]
+        readers = [csv.reader(file, delimiter=self.delimiter) for file in files]
+        iterators = [iter(reader) for reader in readers]
+
+        row_index = 0
+        while row_index < self.line_count:
+            rows = [next(iterator) for iterator in iterators]
+            if row_index == 0:
+                if show_column_names:
+                    print(f'Column names are {rows[0][col1]} and {rows[0][col2]}')
+            else:
+                val1 = statistics.mean([float(row[col1]) for row in rows])
+                val2 = statistics.mean([float(row[col2]) for row in rows])
+                print(f'{val1+x_offset:.2f}\t\t{val2:.4f}\\\\')
+            row_index += 1
+
+
+class SimpleCsv:
+    def __init__(self):
+        self.rows = list()
+
+    def read(self, path: str, delimiter: str=';', encoding: str = 'iso-8859-1', show_read_line_count=False):
+        with open(path, encoding=encoding) as csv_file:
+            reader = csv.reader(csv_file, delimiter=delimiter)
+            self.rows = [row for row in reader]
+
+            if show_read_line_count:
+                print(f'Read {len(self.rows)} lines.')
+
+    def print(self, col1: int=0, col2: int=1, x_offset: float=0, show_column_names: bool=False):
+            line_count = 0
+            for row in self.rows:
+                if line_count == 0:
+                    if show_column_names:
+                        print(f'Column names are {", ".join(row)}')
+                else:
+                    print(f'{float(row[col1])-x_offset}\t\t{row[col2]}\\\\')
+                line_count += 1
--- csv_to_tikz.py
+++ csv_to_tikz.py
@@ -1,5 +1,4 @@
-import csv
-import statistics
+from csv_reader import *
 
 path_origami_03_01V_g = '../Paper1_data/Data/Figure_3/OrigamiHybridization/-0.3-0.1V/Functionalization/ch4e3a-20170329-093634.dyn'
 path_origami_025_015V_g = '../Paper1_data/Data/Figure_3/OrigamiHybridization/-0.25-0.15V/Functionalization/ch4e3a-20170329-103229.dyn'
@@ -54,75 +53,8 @@ path_trm_example = '../Paper1_data/Data/Figure_2/48mer_Switching/ch4e3b-20170330
 path_staple = '../Paper1_data/Data/Figure_3/StapleHybridization/-02V to 02V 1Hz/Regeneration w 6HB_anchor/Functionalization/ch2e3b-20170327-173741.dyn'
 
 
-class VrmPrinter:
-    def __init__(self):
-        self.voltage = None
-        self.fluorescence = list()
-
-        self.voltages = list()
-        self.fluorescences = list()
-
-    def add(self, voltage: float, fluorescence: float):
-        if self.voltage is not None and self.voltage != voltage:
-            self.voltages.append(self.voltage)
-            self.fluorescences.append(statistics.mean(self.fluorescence))
-            self.fluorescence.clear()
-            self.voltage = None
-
-        self.voltage = voltage
-        self.fluorescence.append(fluorescence)
-
-    def print(self):
-        for v, f in zip(self.voltages, self.fluorescences):
-            print(f'{v}\t\t{f}\\\\')
-
-    def print_normalized(self):
-        max_fluorescence = self.fluorescences[0]
-        min_fluorescence = self.fluorescences[-1]
-        for v, f in zip(self.voltages, self.fluorescences):
-            print(f'{v}\t\t{((f-min_fluorescence)/(max_fluorescence-min_fluorescence))}\\\\')
-            # print(f'{((f-min_fluorescence)/(max_fluorescence-min_fluorescence))}')
-
-
-class CsvAverage:
-    def __init__(self, encoding='iso-8859-1', delimiter=';'):
-        self.encoding = encoding
-        self.delimiter = delimiter
-        self.average = list()
-        self.sources = list()
-        self.line_count = None
-
-    def add_source(self, path: str):
-        with open(path, encoding=self.encoding) as file:
-            count = sum(1 for _ in csv.reader(file))
-
-        if self.line_count is None:
-            self.line_count = count
-        elif self.line_count != count:
-            raise Exception("Files must contain same number of lines")
-
-        self.sources.append(path)
-
-    def print(self, col1: int = 0, col2: int = 1, x_offset: float = 0, show_column_names: bool = False):
-        files = [open(source, encoding=self.encoding) for source in self.sources]
-        readers = [csv.reader(file, delimiter=self.delimiter) for file in files]
-        iterators = [iter(reader) for reader in readers]
-
-        row_index = 0
-        while row_index < self.line_count:
-            rows = [next(iterator) for iterator in iterators]
-            if row_index == 0:
-                if show_column_names:
-                    print(f'Column names are {rows[0][col1]} and {rows[0][col2]}')
-            else:
-                val1 = statistics.mean([float(row[col1]) for row in rows])
-                val2 = statistics.mean([float(row[col2]) for row in rows])
-                print(f'{val1+x_offset:.2f}\t\t{val2:.4f}\\\\')
-            row_index += 1
-
-
-vrm_printer = VrmPrinter()
-csv_average = CsvAverage()
+# vrm_printer = VrmPrinter()
+# csv_average = CsvAverage()
 
 # csv_average.add_source(path_96bp_trm_1)
 # csv_average.add_source(path_96bp_trm_2)
@@ -133,27 +65,4 @@ csv_average = CsvAverage()
 # csv_average.print(0, 7)
 
 
-with open(path_48bp_vrr_4, encoding='iso-8859-1') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=';')
-    rows = [row for row in csv_reader]
-    line_count = 0
-    for row in rows:
-        if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
-            line_count += 1
-        else:
-            # amplitude
-            # print(f'{row[0]}\t\t{str(round(float(row[9])-float(row[10]),1))}\\\\')
 
-            # upward motion after downward
-            # print(f'{float(row[0])-100:.2f}\t\t{row[2]}\\\\')
-
-            # vrm
-            vrm_printer.add(float(row[1]), float(row[2]))
-
-            # raw
-            # print(f'{row[0]}\t\t{row[6]}\\\\')
-
-            line_count += 1
-    vrm_printer.print_normalized()
-    print(f'Processed {line_count} lines.')
