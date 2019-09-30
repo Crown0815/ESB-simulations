d62e799ec375642fdbdc243344bcfe4b9aee7c3f
Felix Kröner <felix.kroener@gmx.de>
2019-09-30 21:59:27 +0200

    Add plotting and statistics to interlinking analysis

--- interlinking_analysis.py
+++ interlinking_analysis.py
@@ -2,16 +2,12 @@ from interlinking import Linker
 from surfaces import RandomSurface, HexagonalGrid
 import matplotlib.pyplot as plt
 import tikzplotlib
+import statistics
 
 
 class LinkingSimulation:
     def __init__(self, link_length: float, linkable_length: float = 0, grid_side_length=500,
                  grid_unit_length: float = 2.5, max_links: int = 1):
-        self.surface = None
-        self.linker = None
-        self.links = list()
-        self.linkables = list()
-
         self.link_length = link_length
         self.max_links = max_links
         self.linkable_length = linkable_length
@@ -26,12 +22,17 @@ class LinkingSimulation:
         linker = Linker(surface.coordinates_with_padding, self.max_links, self.linkable_length)
         linker.create_links(self.link_length)
 
-        self.linkables = list(surface.coordinates_within(linker.interlinked,
-                                                         x_max=self.grid_side_length,
-                                                         y_max=self.grid_side_length))
-        self.links = self.links_from(self.linkables)
+        linkables = list(surface.coordinates_within(linker.interlinked,
+                                                    x_max=self.grid_side_length,
+                                                    y_max=self.grid_side_length))
+        return LinkingSimulationResult(surface, linkables)
+
+
+class LinkingSimulationResult:
+    def __init__(self, surface, linkables):
         self.surface = surface
-        self.linker = linker
+        self.linkables = linkables
+        self.links = self.links_from(linkables)
 
     @staticmethod
     def links_from(linkables):
@@ -42,17 +43,14 @@ class LinkingSimulation:
                     links.append(link)
         return links
 
+    def count_of_links(self):
+        return len(self.links)
+
     def count_of_closed_links(self):
         return sum(not link.is_open() for link in self.links)
 
     def count_of_open_links(self):
         return sum(link.is_open() for link in self.links)
-
-    def count_of_linked_linkables(self, linked_threshold: int = 1):
-        return sum(linkable.is_linked(linked_threshold) for linkable in self.linkables)
-
-    def count_of_unlinked_linkables(self, linked_threshold: int = 1):
-        return sum(not linkable.is_linked(linked_threshold) for linkable in self.linkables)
 
     def ratio_closed_links(self):
         return self.count_of_closed_links() / len(self.links)
@@ -60,11 +58,70 @@ class LinkingSimulation:
     def ratio_open_links(self):
         return 1 - self.ratio_closed_links()
 
+    def count_of_linkables(self):
+        return len(self.linkables)
+
+    def count_of_linked_linkables(self, linked_threshold: int = 1):
+        return sum(linkable.is_linked(linked_threshold) for linkable in self.linkables)
+
+    def count_of_unlinked_linkables(self, linked_threshold: int = 1):
+        return sum(not linkable.is_linked(linked_threshold) for linkable in self.linkables)
+
     def ratio_linked_linkables(self, linked_threshold: int = 1):
         return self.count_of_linked_linkables(linked_threshold) / len(self.linkables)
 
     def ratio_unlinked_linkables(self, linked_threshold: int = 1):
         return 1 - self.ratio_linked_linkables(linked_threshold)
+
+
+class LinkingSimulationStatistics:
+    def __init__(self):
+        self.simulation_results = list()
+
+    def append(self, item):
+        self.simulation_results.append(item)
+
+    def count_of_links(self):
+        return self.mean(lambda x: x.count_of_links())
+
+    def count_of_closed_links(self):
+        return self.mean(lambda x: x.count_of_closed_links())
+
+    def count_of_open_links(self):
+        return self.mean(lambda x: x.count_of_open_links())
+
+    def ratio_closed_links(self):
+        return self.mean(lambda x: x.ratio_closed_links())
+
+    def ratio_open_links(self):
+        return self.mean(lambda x: x.ratio_open_links())
+
+    def count_of_linkables(self):
+        return self.mean(lambda x: x.count_of_linkables())
+
+    def count_of_linked_linkables(self, linked_threshold: int = 1):
+        return self.mean(lambda x: x.count_of_linked_linkables(linked_threshold))
+
+    def count_of_unlinked_linkables(self, linked_threshold: int = 1):
+        return self.mean(lambda x: x.count_of_unlinked_linkables(linked_threshold))
+
+    def ratio_linked_linkables(self, linked_threshold: int = 1):
+        return self.mean(lambda x: x.ratio_linked_linkables(linked_threshold))
+
+    def ratio_unlinked_linkables(self, linked_threshold: int = 1):
+        return self.mean(lambda x: x.ratio_unlinked_linkables(linked_threshold))
+
+    def mean_with_stdev(self, resolver):
+        return self.mean(resolver), self.stdev(resolver)
+
+    def mean(self, resolver):
+        return self.calculate_statistics(lambda x: statistics.mean(x), resolver)
+
+    def stdev(self, resolver):
+        return self.calculate_statistics(lambda x: statistics.stdev(x), resolver)
+
+    def calculate_statistics(self, statistic, resolver):
+        return statistic(resolver(x) for x in self.simulation_results)
 
 
 class Analyzer:
@@ -75,14 +132,14 @@ class Analyzer:
         print()
         print("----------------------------")
         print("Linkables:")
-        linkable_count = len(sim.linkables)
+        linkable_count = sim.count_of_linkables()
         print("Linked = {}/{} (ratio: {:.1%})".format(sim.count_of_linked_linkables(), linkable_count,
                                                       sim.ratio_linked_linkables()))
         print("Unlinked = {}/{} (ratio: {:.1%})".format(sim.count_of_unlinked_linkables(), linkable_count,
                                                         sim.ratio_unlinked_linkables()))
 
         print("Links:")
-        link_count = len(sim.links)
+        link_count = sim.count_of_links()
         print("Closed = {}/{} (ratio: {:.1%})".format(sim.count_of_closed_links(), link_count,
                                                       sim.ratio_closed_links()))
         print("Open = {}/{} (ratio: {:.1%})".format(sim.count_of_open_links(), link_count, sim.ratio_open_links()))
@@ -92,6 +149,8 @@ class Analyzer:
         sim = simulation
         figure, axis = sim.surface.visualization()
         dots = list()
+
+        # draw links between interlinked linkables
         for link in sim.links:
             if link.is_open(): continue
             linkables = [link.linked1, link.linked2]
@@ -101,11 +160,13 @@ class Analyzer:
             y_values = sim.surface.y_of(linkables)
             axis.plot(x_values, y_values, color="red")
 
+        # draw non-padding linkables
         dots_to_draw = list(d for d in dots if d in sim.linkables)
         x_values = sim.surface.x_of(dots_to_draw)
         y_values = sim.surface.y_of(dots_to_draw)
         axis.plot(x_values, y_values, 'o', color="red")
 
+        # draw padding linkables
         x_to_draw = list(d for d in dots if d not in sim.linkables)
         x_values = sim.surface.x_of(x_to_draw)
         y_values = sim.surface.y_of(x_to_draw)
@@ -114,13 +175,26 @@ class Analyzer:
         return figure, axis
 
 
-if __name__ == '__main__':
-    linking_simulation = LinkingSimulation(32, 16, 2000, max_links=3)
-
-    for _ in range(1):
-        linking_simulation.run(60)
-        Analyzer.report(linking_simulation)
-        f, ax = Analyzer.visualization(linking_simulation)
-        tikzplotlib.save("./test.tex", figure=f)
-
+def create_surface(grid_size, size, distance, link_length, linkable_length, max_links, figure_size):
+    linking_simulation = LinkingSimulation(link_length, linkable_length, size, grid_size, max_links)
+    result = linking_simulation.run(distance)
+    Analyzer.report(result)
+    Analyzer.visualization(result)
+    tikzplotlib.save("./linking_g{}_d{}_x{}_y{}_l{}_c{}_f{}.tex".format(grid_size, distance, size, size,
+                                                                        link_length + 2 * linkable_length,
+                                                                        max_links, figure_size))
     plt.show()
+
+
+def create_statistics(grid_size, size, distance, link_length, linkable_length, max_links, run_count):
+    simulation = LinkingSimulation(link_length, linkable_length, size, grid_size, max_links)
+    simulation_statistics = LinkingSimulationStatistics()
+    for _ in range(run_count):
+        simulation_statistics.append(simulation.run(distance))
+
+    Analyzer.report(simulation_statistics)
+
+
+if __name__ == '__main__':
+    # create_surface(30, 1000, 60, 28, 16, 3, 5)
+    create_statistics(30, 1000, 60, 28, 16, 3, 2)
