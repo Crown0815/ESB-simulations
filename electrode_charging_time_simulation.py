c39297b182ab3a04f037722063209365a7c09111
Felix Kröner <felix.kroener@gmx.de>
2020-03-01 11:33:23 +0100

    Add charge time simulation to theory chapter

---
+++ electrode_charging_time_simulation.py
@@ -0,0 +1,13 @@
+from math import *
+
+tau = 
+
+
+def relative_electrode_charge(t):
+    return 1 - exp(-t/tau)
+
+
+if __name__ == "__main__":
+    times = [x*0.1 for x in range(0, 101, 1)]  # potential range
+    for time in times:
+        print(f'{time:.2f}\t\t{relative_electrode_charge(time):.4f}\\\\')
